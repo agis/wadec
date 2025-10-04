f4418744c231136624df61f55d447bfff0f8c12f
Agis Anastasopoulos <a@xz0.org>
2025-10-04 20:53:45 +0300

    Use macro for defining `Index` types

--- src/index.rs
+++ src/index.rs
@@ -2,77 +2,25 @@ use crate::parse_u32;
 use anyhow::Result;
 use std::io;
 
-// https://webassembly.github.io/spec/core/syntax/modules.html#indices
-#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
-pub struct TypeIdx(pub u32);
+macro_rules! define_index {
+    ($name:ident) => {
+        #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+        pub struct $name(pub u32);
 
-impl TypeIdx {
-    pub fn read<R: io::Read + ?Sized>(r: &mut R) -> Result<Self> {
-        Ok(Self(parse_u32(r)?))
-    }
+        impl $name {
+            pub fn read<R: io::Read + ?Sized>(r: &mut R) -> Result<Self> {
+                Ok(Self(parse_u32(r)?))
+            }
+        }
+    };
 }
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
-pub struct FuncIdx(pub u32);
-
-impl FuncIdx {
-    pub fn read<R: io::Read + ?Sized>(r: &mut R) -> Result<Self> {
-        Ok(Self(parse_u32(r)?))
-    }
-}
-
-#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
-pub struct TableIdx(pub u32);
-
-impl TableIdx {
-    pub fn read<R: io::Read + ?Sized>(r: &mut R) -> Result<Self> {
-        Ok(Self(parse_u32(r)?))
-    }
-}
-
-#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
-pub struct MemIdx(pub u32);
-
-#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
-pub struct GlobalIdx(pub u32);
-impl GlobalIdx {
-    pub fn read<R: io::Read + ?Sized>(r: &mut R) -> Result<Self> {
-        Ok(Self(parse_u32(r)?))
-    }
-}
-
-#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
-pub struct ElemIdx(pub u32);
-
-impl ElemIdx {
-    pub fn read<R: io::Read + ?Sized>(r: &mut R) -> Result<Self> {
-        Ok(Self(parse_u32(r)?))
-    }
-}
-
-#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
-pub struct DataIdx(pub u32);
-
-impl DataIdx {
-    pub fn read<R: io::Read + ?Sized>(r: &mut R) -> Result<Self> {
-        Ok(Self(parse_u32(r)?))
-    }
-}
-
-#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
-pub struct LocalIdx(pub u32);
-
-impl LocalIdx {
-    pub fn read<R: io::Read + ?Sized>(r: &mut R) -> Result<Self> {
-        Ok(Self(parse_u32(r)?))
-    }
-}
-
-#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
-pub struct LabelIdx(pub u32);
-
-impl LabelIdx {
-    pub fn read<R: io::Read + ?Sized>(r: &mut R) -> Result<Self> {
-        Ok(Self(parse_u32(r)?))
-    }
-}
+define_index!(TypeIdx);
+define_index!(FuncIdx);
+define_index!(TableIdx);
+define_index!(MemIdx);
+define_index!(GlobalIdx);
+define_index!(ElemIdx);
+define_index!(DataIdx);
+define_index!(LocalIdx);
+define_index!(LabelIdx);
