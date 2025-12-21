agis/wadec

commit 9f8dd99a6b6172b9bc77a843f50678373d458d09

Implement `FromMarkerByte` for `SectionKind`

src/lib.rs
@@ -592,14 +592,18 @@ pub enum SectionKind {
 }
 
 #[derive(Debug, Error)]
-#[error("invalid section ID: expected 0-12; got {0}")]
+#[error("invalid section ID: expected one of {markers}; got {0:#04X}", markers=SectionKind::markers_formatted())]
 pub struct InvalidSectionIdError(pub u8);
 
-impl TryFrom<u8> for SectionKind {
-    type Error = InvalidSectionIdError;
+impl From<u8> for InvalidSectionIdError {
+    fn from(b: u8) -> Self {
+        Self(b)
+    }
+}
 
-    fn try_from(v: u8) -> Result<Self, Self::Error> {
-        Ok(match v {
+// Valid marker bytes for [SectionKind].
+#[expect(non_upper_case_globals)]
+static SectionId_MARKERS: phf::OrderedMap<u8, SectionKind> = phf_ordered_map! {
             0 => SectionKind::Custom,
             1 => SectionKind::Type,
             2 => SectionKind::Import,
@@ -613,8 +617,13 @@ impl TryFrom<u8> for SectionKind {
             10 => SectionKind::Code,
             11 => SectionKind::Data,
             12 => SectionKind::DataCount,
-            n => return Err(InvalidSectionIdError(n)),
-        })
+};
+
+impl FromMarkerByte for SectionKind {
+    type Error = InvalidSectionIdError;
+
+    fn markers() -> &'static phf::OrderedMap<u8, Self> {
+        &SectionId_MARKERS
     }
 }
 
@@ -948,7 +957,7 @@ fn decode_section_header<R: Read + ?Sized>(
         Err(e) => return Err(e.into()),
     };
 
-    let kind = SectionKind::try_from(id)?;
+    let kind = SectionKind::from_marker(id)?;
     let size = read_u32(reader)?;
 
     Ok(Some(SectionHeader { kind, size }))

commit a9faf7d13392152eba8d9751f5b1427f244c0764

Fix section header parsing regression from 9f8dd99

src/lib.rs
@@ -613,19 +613,19 @@ impl From<u8> for InvalidSectionIdError {
 // Valid marker bytes for [SectionKind].
 #[expect(non_upper_case_globals)]
 static SectionId_MARKERS: phf::OrderedMap<u8, SectionKind> = phf_ordered_map! {
-            0 => SectionKind::Custom,
-            1 => SectionKind::Type,
-            2 => SectionKind::Import,
-            3 => SectionKind::Function,
-            4 => SectionKind::Table,
-            5 => SectionKind::Memory,
-            6 => SectionKind::Global,
-            7 => SectionKind::Export,
-            8 => SectionKind::Start,
-            9 => SectionKind::Element,
-            10 => SectionKind::Code,
-            11 => SectionKind::Data,
-            12 => SectionKind::DataCount,
+            0u8 => SectionKind::Custom,
+            1u8 => SectionKind::Type,
+            2u8 => SectionKind::Import,
+            3u8 => SectionKind::Function,
+            4u8 => SectionKind::Table,
+            5u8 => SectionKind::Memory,
+            6u8 => SectionKind::Global,
+            7u8 => SectionKind::Export,
+            8u8 => SectionKind::Start,
+            9u8 => SectionKind::Element,
+            10u8 => SectionKind::Code,
+            11u8 => SectionKind::Data,
+            12u8 => SectionKind::DataCount,
 };
 
 impl FromMarkerByte for SectionKind {